f93943d36b6d522ffc3ac4cea8f295299fda623b
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2025-11-19 16:24:20 +0100

    added basic mlp

---
+++ model.py
@@ -0,0 +1,41 @@
+from nn import Linear
+from tensor import Tensor
+
+
+class MLP:
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+        num_hidden: int = 2,
+        hidden_size: int = 128,
+    ):
+        self.input_dim, self.output_dim, self.num_hidden, self.hidden_size = (
+            input_dim,
+            output_dim,
+            num_hidden,
+            hidden_size,
+        )
+        self.input_layer = Linear(input_dim, hidden_size)
+        assert num_hidden > 0, "Number of hidden layers must be positive"
+        self.hidden_layers = [
+            Linear(hidden_size, hidden_size) for _ in range(num_hidden - 1)
+        ]
+        self.output_layer = Linear(hidden_size, output_dim)
+
+    def forward(self, x: Tensor) -> Tensor:
+        ret = self.input_layer.forward(x)
+        ret = ret.lrelu()
+        for layer in self.hidden_layers:
+            ret = layer.forward(ret)
+            ret = ret.lrelu()
+        output = self.output_layer.forward(ret)
+        return output
+
+    def parameters(self) -> list[Tensor]:
+        params = []
+        params.extend(self.input_layer.parameters())
+        for layer in self.hidden_layers:
+            params.extend(layer.parameters())
+        params.extend(self.output_layer.parameters())
+        return params
